a45c17611e1e63c48a219af729b70d08090fca68
Zoe <ezoe013@gmail.com>
2020-06-12 20:10:37 +0100

    Got adding a photo to work

--- comic/views.py
+++ comic/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Page
+from django.utils import timezone
 
 # Create your views here.
+
+
+def page_list(request):
+	pages = Page.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'comic/page_list.html', {'pages':pages})
