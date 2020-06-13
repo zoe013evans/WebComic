448265fc7e633c50180bea7ec6eec54e58c194ea
Zoe <ezoe013@gmail.com>
2020-06-13 20:15:15 +0100

    Adding single page view

--- comic/urls.py
+++ comic/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.page_list), 
+	path('page/<int:pk>/', views.page_detail, name='page_detail'),
 ]
--- comic/views.py
+++ comic/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Page
 from django.utils import timezone
+
 
 # Create your views here.
 
@@ -8,3 +9,13 @@ from django.utils import timezone
 def page_list(request):
 	pages = Page.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'comic/page_list.html', {'pages':pages})
+
+
+
+
+
+def page_detail(request, pk): 
+	page = get_object_or_404(Page, pk=pk)
+	return render(request, 'comic/page_detail.html', {'page': page})
+
+
